04c6c8056deb8d57246278f324c4fc75c09d70b1
Dan Mee <danmee10@gmail.com>
2014-06-17 15:34:46 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SandboxDan::Application.config.secret_key_base = '7953534da5d892f33a7c7fe36196cbfbe78d7e732a6ba643b5db6020f6a581301e55468f5954b5b1556ef3db04d1df9255049d08272cb6bdc1a3cfba231e1a5d'
